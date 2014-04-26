15361b57f447a6136fb879cf4e10a403909db7e9
Larapriar <larapriar@gmail.com>
2014-04-26 02:12:53 +0200

    login

--- Persistencia y Presentacion/persistenciaYpresentacion/app/controllers/Application.java
+++ Persistencia y Presentacion/persistenciaYpresentacion/app/controllers/Application.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import java.util.Map;
+
 import models.Observation;
 import models.User;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utils.Volcado;
 import views.html.discover;
 import views.html.index;
 import views.html.login;
@@ -27,32 +30,34 @@ public class Application extends Controller {
 	public static Result idioma() {
 		return TODO;
 	}
-	
+
 	public static Result login(){
 		return ok(login.render(Form.form(User.class)));
 	}
-	
+
 	public static Result discover(){
 		return ok(discover.render());
 	}
-	
+
 	public static Result organizations(){
 		return ok(organizations.render());
 	}
-	
+
 	public static Result authenticate() {
-		return TODO;
-		
-		/*
-	    Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
-	    if (loginForm.hasErrors()) {
-	        return badRequest(login.render(loginForm));
-	    } else {
-	        session().clear();
-	        session("user", loginForm.get().getId());
-	        return redirect(
-	            routes.Application.index()
-	        );
-	    }*/
+		Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest("algo has hecho mal");
+		}else{
+			Map<String, String[]> values = request().body().asFormUrlEncoded();
+			String email = values.get("emailLog")[0];
+			String pass = values.get("passLog")[0];
+
+			User us = User.findByEmail(email);
+			Volcado.getInstance().volcar(email,pass);
+			if(us != null && us.getPassword().equalsIgnoreCase(pass))
+				return redirect(routes.Application.index());
+			else
+				return redirect(routes.Application.login());
+		}
 	}
 }
---
+++ Persistencia y Presentacion/persistenciaYpresentacion/app/controllers/Login.java
@@ -0,0 +1,20 @@
+package controllers;
+
+public class Login {
+
+	private String email;
+	private String password;
+	
+	
+
+	public String getEmail() {
+		
+		return this.email;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+
+
+}
