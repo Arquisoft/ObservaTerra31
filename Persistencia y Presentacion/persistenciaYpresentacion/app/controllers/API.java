a286c6ea1fbf1a72c280fd32ffba7d8e6b3ffaa8
Sergio <sergiorodriguezgijon@gmail.com>
2014-04-27 19:08:41 +0200

    API mejorada

--- Persistencia y Presentacion/persistenciaYpresentacion/app/controllers/API.java
+++ Persistencia y Presentacion/persistenciaYpresentacion/app/controllers/API.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 import models.Indicator;
 import models.Observation;
+import models.Organization;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.Jsonin;
 import views.html.ajaxE;
 
 public class API extends Controller {
-	
-	
+
 	public static Result allObservations() {
-		/*List<Observation> obs = PersistenceSimulator.getInstance()
-				.getObservations();
-		String ret = Jsonin.observations2json(obs);
-		return ok(ret);*/
-		
+		/*
+		 * List<Observation> obs = PersistenceSimulator.getInstance()
+		 * .getObservations(); String ret = Jsonin.observations2json(obs);
+		 * return ok(ret);
+		 */
+
 		List<Observation> obs = Observation.all();
 		String ret = Jsonin.observations2json(obs);
 		return ok(ret);
@@ -25,43 +26,38 @@ public class API extends Controller {
 
 	public static Result observationsByIndicator(String indicator) {
 		/*
-		List<Observation> obs = PersistenceSimulator.getInstance()
-				.findObservations(indicator);
-		String ret = Jsonin.observations2json(obs);
-		return ok(ret);
-		*/
+		 * List<Observation> obs = PersistenceSimulator.getInstance()
+		 * .findObservations(indicator); String ret =
+		 * Jsonin.observations2json(obs); return ok(ret);
+		 */
 		Indicator ind = Indicator.findById(Long.parseLong(indicator));
 		List<Observation> obs = Observation.findByIndicator(ind);
 		String ret = Jsonin.observations2json(obs);
 		return ok(ret);
 	}
 
-	public static Result observationsByYear(String year) {
-		return TODO;
-	}
-
 	public static Result observationsByProvider(String providerName) {
-		return TODO;
+		Organization org = Organization.findByName(providerName);
+		List<Observation> obs = Observation.findByProvider(org);
+		String ret = Jsonin.observations2json(obs);
+		return ok(ret);
 	}
 
-	public static Result observationsByArea(String areaName) {
-		return TODO;
+	public static Result allOrganizations() {
+		List<Organization> orgs = Organization.findAll();
+		String ret = Jsonin.organizations2json(orgs);
+		return ok(ret);
+
 	}
-	
-	public static Result allOrganizations(){
-		return TODO;
+
+	public static Result arganizationsByName(String name) {
+		Organization org = Organization.findByName(name);
+		return ok(org.toJson());
 	}
-	
-	public static Result arganizations(){
-		return TODO;
-	}
-	
-	public static Result arganizationsByName(String name){
-		return TODO;
-	}
-	
-	public static Result arganizationsByNumberOfMembers(int membersCount){
-		return TODO;
+
+	public static Result arganizationsByID(Long id) {
+		Organization org = Organization.findById(id); 
+		return ok (org.toJson());
 	}
 
 	public static Result ajax() {
--- Persistencia y Presentacion/persistenciaYpresentacion/app/utils/Jsonin.java
+++ Persistencia y Presentacion/persistenciaYpresentacion/app/utils/Jsonin.java
@@ -3,6 +3,7 @@ package utils;
 import java.util.List;
 
 import models.Observation;
+import models.Organization;
 
 public class Jsonin {
 	
@@ -12,6 +13,18 @@ public class Jsonin {
 		sb.append("{ \"observations\": [");
 		
 		for (Observation ob : collection)
+			sb.append(ob.toJson()).append(",");
+		
+		sb.deleteCharAt(sb.length()-1).append("]}");
+		return sb.toString();
+		
+	}
+	
+	public static String organizations2json(List<Organization> collection) {
+		StringBuilder sb = new StringBuilder ();
+		sb.append("{ \"organizations \": [");
+		
+		for (Organization ob : collection)
 			sb.append(ob.toJson()).append(",");
 		
 		sb.deleteCharAt(sb.length()-1).append("]}");
