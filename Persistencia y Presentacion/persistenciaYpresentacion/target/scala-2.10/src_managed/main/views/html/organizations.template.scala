c02d2f8fa4eed75475ce5927e6b55e85dffe3dc2
Larapriar <larapriar@gmail.com>
2014-05-01 17:49:42 +0200

    organizaciones centrado

--- Persistencia y Presentacion/persistenciaYpresentacion/target/scala-2.10/src_managed/main/views/html/organizations.template.scala
+++ Persistencia y Presentacion/persistenciaYpresentacion/target/scala-2.10/src_managed/main/views/html/organizations.template.scala
@@ -31,16 +31,17 @@ Seq[Any](format.raw/*1.37*/("""
 """),format.raw/*3.1*/("""
 """),_display_(Seq[Any](/*4.2*/navbarGuest/*4.13*/ {_display_(Seq[Any](format.raw/*4.15*/("""
 
-	<div class="list-group" id="organizations">
-	<h2>"""),_display_(Seq[Any](/*7.7*/Messages/*7.15*/.get("org"))),format.raw/*7.26*/("""</h2>
-	"""),_display_(Seq[Any](/*8.3*/for(org <- organizations) yield /*8.28*/{_display_(Seq[Any](format.raw/*8.29*/("""
+	<div class="list-group" id="organizations"><center>
+	
+	<h2>"""),_display_(Seq[Any](/*8.7*/Messages/*8.15*/.get("org"))),format.raw/*8.26*/("""</h2>
+	"""),_display_(Seq[Any](/*9.3*/for(org <- organizations) yield /*9.28*/{_display_(Seq[Any](format.raw/*9.29*/("""
 	<a href="#"
 					class="list-group-item" 
 					data-toggle="modal">
-					<h5 class="list-group-item-heading">"""),_display_(Seq[Any](/*12.43*/org)),format.raw/*12.46*/("""</h5>
-				</a> """)))})),format.raw/*13.11*/("""
+					<h5 class="list-group-item-heading">"""),_display_(Seq[Any](/*13.43*/org)),format.raw/*13.46*/("""</h5>
+				</a> """)))})),format.raw/*14.11*/("""
 	
-	
+	</center>
 	</div>
 	
 
@@ -56,11 +57,11 @@ Seq[Any](format.raw/*1.37*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu May 01 17:41:32 CEST 2014
+                    DATE: Thu May 01 17:48:53 CEST 2014
                     SOURCE: C:/Users/Lara/Documents/GitHub/ObservaTerra31/Persistencia y Presentacion/persistenciaYpresentacion/app/views/organizations.scala.html
-                    HASH: 1237b5f3d373c45b1ddf877b03ccdf6aa548ff85
-                    MATRIX: 794->1|950->36|979->67|1016->70|1035->81|1074->83|1164->139|1180->147|1212->158|1255->167|1295->192|1333->193|1485->309|1510->312|1559->329
-                    LINES: 26->1|30->1|31->3|32->4|32->4|32->4|35->7|35->7|35->7|36->8|36->8|36->8|40->12|40->12|41->13
+                    HASH: dc890d2d44e19a697191f560bc99f7ca6037f80f
+                    MATRIX: 794->1|950->36|979->67|1016->70|1035->81|1074->83|1175->150|1191->158|1223->169|1266->178|1306->203|1344->204|1496->320|1521->323|1570->340
+                    LINES: 26->1|30->1|31->3|32->4|32->4|32->4|36->8|36->8|36->8|37->9|37->9|37->9|41->13|41->13|42->14
                     -- GENERATED --
                 */
             
